04e67cc65e494a5cddf3411ab1daaf43c03ad850
Amrutharaj <amrutha@zelarsoft.com>
2021-05-29 15:16:30 +0530

    update

---
+++ plugins.groovy
@@ -0,0 +1,30 @@
+/* gitlab-plugin,hipchat&swarm are plugin names
+* gilab-plugin : seed
+* hipchat :authorize-project
+* swarm :git (optional)
+*/
+
+import jenkins.model.*
+
+def pluginParameter="gitlab-plugin hipchat swarm"
+def plugins = pluginParameter.split()
+println(plugins)
+def instance = Jenkins.getInstance()
+def pm = instance.getPluginManager()
+def uc = instance.getUpdateCenter()
+def installed = false
+
+plugins.each {
+  if (!pm.getPlugin(it)) {
+    def plugin = uc.getPlugin(it)
+    if (plugin) {
+      println("Installing " + it)
+      plugin.deploy()
+      installed = true
+    }
+  }
+}
+
+instance.save()
+if (installed)
+instance.doSafeRestart()
