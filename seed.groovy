8a1497763aa7e947ea638b14e49e70d6b911b6c1
Amrutharaj <amrutha@zelarsoft.com>
2021-06-01 13:27:15 +0530

    Update jenkinsfile with folder

--- seed.groovy
+++ seed.groovy
@@ -4,28 +4,30 @@
 // scriptPath : Jenkinsfile
 
 pipelineJob('frontend-ci') {
-  folder('TODO_CI Pipelines') {
-    displayName('TODO_CI Pipelines')
-    description('TODO_CI Pipelines')
+  folder('TODO_CI-Pipelines') {
+    displayName('TODO_CI-Pipelines')
+    description('TODO_CI-Pipelines')
   }
 
-pipelineJob('TODO_CI Pipelines/frontend-ci') {
-  configure { flowdefinition ->
-    flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
-      'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
-        'userRemoteConfigs' {
-          'hudson.plugins.git.UserRemoteConfig' {
-            'url'('https://github.com/zs-amrutha/frontend.git')
+
+  pipelineJob('TODO_CI-Pipelines/frontend-ci') {
+    configure { flowdefinition ->
+      flowdefinition << delegate.'definition'(class: 'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition', plugin: 'workflow-cps') {
+        'scm'(class: 'hudson.plugins.git.GitSCM', plugin: 'git') {
+          'userRemoteConfigs' {
+            'hudson.plugins.git.UserRemoteConfig' {
+              'url'('https://github.com/zsdevops01/frontend.git')
+            }
+          }
+          'branches' {
+            'hudson.plugins.git.BranchSpec' {
+              'name'('*/main')
+            }
           }
         }
-        'branches' {
-          'hudson.plugins.git.BranchSpec' {
-            'name'('*/main')
-          }
-        }
+        'scriptPath'('Jenkinsfile')
+        'lightweight'(true)
       }
-      'scriptPath'('Jenkinsfile')
-      'lightweight'(true)
     }
   }
 }
