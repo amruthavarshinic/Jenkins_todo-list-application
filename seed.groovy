abd4dd3c316c2bb43133e9ad3c660be92411aec6
Amrutharaj <amrutha@zelarsoft.com>
2021-06-01 13:39:28 +0530

    update jenkinsfile

--- seed.groovy
+++ seed.groovy
@@ -9,14 +9,13 @@ pipelineJob('frontend-ci') {
     description('TODO_CI-Pipelines')
   }
 
-
   pipelineJob('TODO_CI-Pipelines/frontend-ci') {
     configure { flowdefinition ->
       flowdefinition << delegate.'definition'(class: 'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition', plugin: 'workflow-cps') {
         'scm'(class: 'hudson.plugins.git.GitSCM', plugin: 'git') {
           'userRemoteConfigs' {
             'hudson.plugins.git.UserRemoteConfig' {
-              'url'('https://github.com/zs-amrutha/frontend.git')
+              'url'('https://github.com/zs-amrutha/frontend')
             }
           }
           'branches' {
