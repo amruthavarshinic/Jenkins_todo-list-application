71746403a87bea4c74eb164abf431ea45c29f2d2
Amrutharaj <amrutha@zelarsoft.com>
2021-06-02 11:52:38 +0530

    ss

--- var/nexus.groovy
+++
@@ -1,4 +0,0 @@
-def nexus() {
-    command = "curl -f -v -u admin:admin123 --upload-file /home/ubuntu/workspace/TODO_CI-Pipelines/todo.zip http://172.31.52.12:8081/repository/todo/todo1.zip"
-    def execute_state=sh(returnStdout: true, script: command)
-}
