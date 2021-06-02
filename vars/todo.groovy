80bf3d10ed07e132fbcb5891726cf130893463b6
Amrutharaj <amrutha@zelarsoft.com>
2021-06-02 15:03:31 +0530

    update frontend and todo

--- vars/todo.groovy
+++ vars/todo.groovy
@@ -3,14 +3,16 @@ def call(Map params = [:]) {
   def args = [
           NEXUS_IP               : '172.31.52.12',
   ]
+  
   args << params
-
+  
   // End Default + Required Arguments
+ 
   pipeline {
     agent {
       label "${args.SLAVE_LABEL}"
     }
-
+    
     environment {
       COMPONENT     = "${args.COMPONENT}"
       NEXUS_IP      = "${args.NEXUS_IP}"
@@ -19,34 +21,48 @@ def call(Map params = [:]) {
       APP_TYPE      = "${args.APP_TYPE}"
     }
 
-    stage('Downloading Dependencies') {
-      when {
+    stages {
+
+      stage('Prepare Artifacts') {
+      stage('Prepare Artifacts - forntend') {
+        when {
           environment name: 'APP_TYPE', value: 'NODEJS'
+        }
+        steps {
+          sh '''
+          cd static
+          zip -r ../${COMPONENT}.zip node_modules server.js
+        '''
+        }
       }
-      steps {
-        sh '''
+      stage('Download Dependencies') {
+        when {
+          environment name: 'APP_TYPE', value: 'NODEJS'
+        }
+        steps {
+          sh '''
           npm install
         '''
+        }
       }
-    }
-    
-    stage('Prepare Artifacts') {
-      when {
-          environment name: 'APP_TYPE', value: 'NODEJS'      
-      }
-      steps {
-        sh '''
-          zip ../${COMPONENT}.zip *
-        '''
-      }
-    }
 
-    stage('Upload Artifact') {
-      steps {
-        script {
-           nexus
+      stage('Prepare Artifacts - todo') {
+        when {
+          environment name: 'APP_TYPE', value: 'NODEJS'
+        }
+        steps {
+          sh '''
+          zip -r ${COMPONENT}.zip node_modules server.js
+        '''
+        }
+      }
+      stage('Upload Artifacts') {
+        steps {
+          sh '''
+          curl -f -v -u admin:admin123 --upload-file frontend.zip http://${NEXUS_IP}:8081/repository/frontend/frontend.zip
+        '''
         }
       }
     }
-  } 
-} 
+  }
+}
