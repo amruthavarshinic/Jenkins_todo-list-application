89eac2215bae2e1fe2de03e5e8c49a14ac371b01
Amrutharaj <amrutha@zelarsoft.com>
2021-06-03 18:38:32 +0530

    update seed file for refsec nd code dry

--- seed.groovy
+++ seed.groovy
@@ -3,87 +3,126 @@
 // brach name : */main 
 // scriptPath : Jenkinsfile
 
-pipelineJob('frontend-ci') {
-  folder('TODO_CI-Pipelines') {
-    displayName('TODO_CI-Pipelines')
-    description('TODO_CI-Pipelines')
-  }
+// pipelineJob('frontend-ci') {
+//   folder('TODO_CI-Pipelines') {
+//     displayName('TODO_CI-Pipelines')
+//     description('TODO_CI-Pipelines')
+//   }
 
-  pipelineJob('TODO_CI-Pipelines/frontend-ci') {
+//   pipelineJob('TODO_CI-Pipelines/frontend-ci') {
+//     configure { flowdefinition ->
+//       flowdefinition << delegate.'definition'(class: 'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition', plugin: 'workflow-cps') {
+//         'scm'(class: 'hudson.plugins.git.GitSCM', plugin: 'git') {
+//           'userRemoteConfigs' {
+//             'hudson.plugins.git.UserRemoteConfig' {
+//               'url'('https://github.com/zs-amrutha/frontend.git')
+//             }
+//           }
+//           'branches' {
+//             'hudson.plugins.git.BranchSpec' {
+//               'name'('*/main')
+//             }
+//           }
+//         }
+//         'scriptPath'('Jenkinsfile')
+//         'lightweight'(true)
+//       }
+//     }
+//   }
+
+//   pipelineJob('TODO_CI-Pipelines/todo-ci') {
+//     configure { flowdefinition ->
+//       flowdefinition << delegate.'definition'(class: 'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition', plugin: 'workflow-cps') {
+//         'scm'(class: 'hudson.plugins.git.GitSCM', plugin: 'git') {
+//           'userRemoteConfigs' {
+//             'hudson.plugins.git.UserRemoteConfig' {
+//               'url'('https://github.com/zs-amrutha/todo.git')
+//             }
+//           }
+//           'branches' {
+//             'hudson.plugins.git.BranchSpec' {
+//               'name'('*/main')
+//             }
+//           }
+//         }
+//         'scriptPath'('Jenkinsfile')
+//         'lightweight'(true)
+//       }
+//     }
+//   }
+
+//   pipelineJob('TODO_CI-Pipelines/login-ci') {
+//     configure { flowdefinition ->
+//       flowdefinition << delegate.'definition'(class: 'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition', plugin: 'workflow-cps') {
+//         'scm'(class: 'hudson.plugins.git.GitSCM', plugin: 'git') {
+//           'userRemoteConfigs' {
+//             'hudson.plugins.git.UserRemoteConfig' {
+//               'url'('https://github.com/zs-amrutha/login.git')
+//             }
+//           }
+//           'branches' {
+//             'hudson.plugins.git.BranchSpec' {
+//               'name'('*/main')
+//             }
+//           }
+//         }
+//         'scriptPath'('Jenkinsfile')
+//         'lightweight'(true)
+//       }
+//     }
+//   }
+
+//   pipelineJob('TODO_CI-Pipelines/users-ci') {
+//     configure { flowdefinition ->
+//       flowdefinition << delegate.'definition'(class: 'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition', plugin: 'workflow-cps') {
+//         'scm'(class: 'hudson.plugins.git.GitSCM', plugin: 'git') {
+//           'userRemoteConfigs' {
+//             'hudson.plugins.git.UserRemoteConfig' {
+//               'url'('https://github.com/zs-amrutha/users.git')
+//             }
+//           }
+//           'branches' {
+//             'hudson.plugins.git.BranchSpec' {
+//               'name'('*/main')
+//             }
+//           }
+//         }
+//         'scriptPath'('Jenkinsfile')
+//         'lightweight'(true)
+//       }
+//     }
+//   }
+// }
+
+folder('TODO_CI-Pipelines') {
+  displayName('TODO_CI-Pipeliness')
+  description('TODO_CI-Pipelines')
+}
+def component = ["todo", "login","users","frontend"];
+def count=(component.size()-1)
+for (i in 0..count) {
+  def j=component[i]
+  pipelineJob("CI-Pipelines/${j}-ci") {
     configure { flowdefinition ->
-      flowdefinition << delegate.'definition'(class: 'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition', plugin: 'workflow-cps') {
-        'scm'(class: 'hudson.plugins.git.GitSCM', plugin: 'git') {
-          'userRemoteConfigs' {
-            'hudson.plugins.git.UserRemoteConfig' {
-              'url'('https://github.com/zs-amrutha/frontend.git')
-            }
-          }
-          'branches' {
-            'hudson.plugins.git.BranchSpec' {
-              'name'('*/main')
-            }
+      flowdefinition / 'properties' << 'org.jenkinsci.plugins.workflow.job.properties.PipelineTriggersJobProperty' {
+        'triggers' {
+          'hudson.triggers.SCMTrigger' {
+            'spec'('*/2 * * * 1-5')
+            'ignorePostCommitHooks'(false)
           }
         }
-        'scriptPath'('Jenkinsfile')
-        'lightweight'(true)
       }
-    }
-  }
-
-  pipelineJob('TODO_CI-Pipelines/todo-ci') {
-    configure { flowdefinition ->
-      flowdefinition << delegate.'definition'(class: 'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition', plugin: 'workflow-cps') {
-        'scm'(class: 'hudson.plugins.git.GitSCM', plugin: 'git') {
+      flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
+        'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
           'userRemoteConfigs' {
             'hudson.plugins.git.UserRemoteConfig' {
-              'url'('https://github.com/zs-amrutha/todo.git')
+              'url'('https://github.com/zsdevops01/'+j+'.git')
+              'refspec'('\'+refs/tags/*\':\'refs/remotes/origin/tags/*\'')
             }
           }
           'branches' {
             'hudson.plugins.git.BranchSpec' {
-              'name'('*/main')
-            }
-          }
-        }
-        'scriptPath'('Jenkinsfile')
-        'lightweight'(true)
-      }
-    }
-  }
-
-  pipelineJob('TODO_CI-Pipelines/login-ci') {
-    configure { flowdefinition ->
-      flowdefinition << delegate.'definition'(class: 'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition', plugin: 'workflow-cps') {
-        'scm'(class: 'hudson.plugins.git.GitSCM', plugin: 'git') {
-          'userRemoteConfigs' {
-            'hudson.plugins.git.UserRemoteConfig' {
-              'url'('https://github.com/zs-amrutha/login.git')
-            }
-          }
-          'branches' {
-            'hudson.plugins.git.BranchSpec' {
-              'name'('*/main')
-            }
-          }
-        }
-        'scriptPath'('Jenkinsfile')
-        'lightweight'(true)
-      }
-    }
-  }
-
-  pipelineJob('TODO_CI-Pipelines/users-ci') {
-    configure { flowdefinition ->
-      flowdefinition << delegate.'definition'(class: 'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition', plugin: 'workflow-cps') {
-        'scm'(class: 'hudson.plugins.git.GitSCM', plugin: 'git') {
-          'userRemoteConfigs' {
-            'hudson.plugins.git.UserRemoteConfig' {
-              'url'('https://github.com/zs-amrutha/users.git')
-            }
-          }
-          'branches' {
-            'hudson.plugins.git.BranchSpec' {
-              'name'('*/main')
+              'name'('*/tags/*')
             }
           }
         }
